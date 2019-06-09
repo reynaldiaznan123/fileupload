e75c58fa9f55694e5d0836e439f18fd007f11410
Reynaldi Aznan <reynaldiaznan450@gmail.com>
2019-06-09 07:56:27 -0700

    Init commit

---
+++ application/controllers/Fileupload.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Fileupload extends CI_Controller {
+    public function index() {
+        $config['upload_path']          = './uploads/';
+        $config['allowed_types']        = 'gif|jpg|png';
+        $config['max_size']             = 100;
+        $config['max_width']            = 1024;
+        $config['max_height']           = 768;
+
+        $this->load->library('upload', $config);
+
+        if ( ! $this->upload->do_upload('file_image')) {
+            $error = array('error' => $this->upload->display_errors());
+
+            //$this->load->view('upload_form', $error);
+            echo json_encode($error);
+        } else {
+            $data = array('upload_data' => $this->upload->data());
+
+            // $this->load->view('upload_success', $data);
+            echo json_encode($data);
+        }
+    }
+}
---
+++ application/views/welcome_message.php
@@ -0,0 +1,147 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+	<style type="text/css">
+
+	::selection { background-color: #E13300; color: white; }
+	::-moz-selection { background-color: #E13300; color: white; }
+
+	body {
+		background-color: #fff;
+		margin: 40px;
+		font: 13px/20px normal Helvetica, Arial, sans-serif;
+		color: #4F5155;
+	}
+
+	a {
+		color: #003399;
+		background-color: transparent;
+		font-weight: normal;
+	}
+
+	h1 {
+		color: #444;
+		background-color: transparent;
+		border-bottom: 1px solid #D0D0D0;
+		font-size: 19px;
+		font-weight: normal;
+		margin: 0 0 14px 0;
+		padding: 14px 15px 10px 15px;
+	}
+
+	code {
+		font-family: Consolas, Monaco, Courier New, Courier, monospace;
+		font-size: 12px;
+		background-color: #f9f9f9;
+		border: 1px solid #D0D0D0;
+		color: #002166;
+		display: block;
+		margin: 14px 0 14px 0;
+		padding: 12px 10px 12px 10px;
+	}
+
+	#body {
+		margin: 0 15px 0 15px;
+	}
+
+	p.footer {
+		text-align: right;
+		font-size: 11px;
+		border-top: 1px solid #D0D0D0;
+		line-height: 32px;
+		padding: 0 10px 0 10px;
+		margin: 20px 0 0 0;
+	}
+
+	#container {
+		margin: 10px;
+		border: 1px solid #D0D0D0;
+		box-shadow: 0 0 8px #D0D0D0;
+	}
+	</style>
+</head>
+<body>
+
+<div id="container">
+	<h1>Welcome to CodeIgniter!</h1>
+
+	<div id="body">
+		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+
+		<p>If you would like to edit this page you'll find it located at:</p>
+		<code>application/views/welcome_message.php</code>
+
+		<p>The corresponding controller for this page is found at:</p>
+		<code>application/controllers/Welcome.php</code>
+
+		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+	</div>
+
+	<div>
+		<input type="file" id="document">
+		<button type="button" class="btn btn-primary" id="upload">Upload</button>
+		<div class="progress">
+			<div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+		</div>
+	</div>
+
+	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+</div>
+
+<script src="<?= base_url('asset/js/jquery.min.js') ?>"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script>
+$(function() {
+	$('.progress').hide();
+
+	$('#upload').click(function(e) {
+		const formData = new FormData();
+		formData.append('file_image', $('#document')[0].files[0]);
+
+		
+		$.ajax({
+			xhr : function() {
+				var xhr = new window.XMLHttpRequest();
+				xhr.upload.addEventListener('progress', function(e){
+					if(e.lengthComputable){
+						console.log('Bytes Loaded : ' + e.loaded);
+						console.log('Total Size : ' + e.total);
+						console.log('Persen : ' + (e.loaded / e.total));
+						
+						var percent = Math.round((e.loaded / e.total) * 100);
+						
+						$('.progress .progress-bar').attr('aria-valuenow', percent).css('width', percent + '%').text(percent + '%');
+					}
+				});
+				return xhr;
+			},
+			beforeSend: function() {
+				$('.progress').show();
+			},
+			type : 'POST',
+			url : '<?= base_url('index.php/fileupload') ?>',
+			data : formData,
+			processData : false,
+			contentType : false,
+			success : function(response){
+				$('.progress').hide();
+				if (response == "") {
+					alert('File gagal di upload');
+				} else {
+					alert('File berhasil di upload. ID file = ' + response);
+				}
+			}
+		});
+	});
+});
+</script>
+</body>
+</html>
